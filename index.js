66a42292ad4876f251814b2abe4467a94131c079
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:39:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const cors = require('koa-cors');
 const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000
 const apiKey = 'live_c4cf0b8b2a095663e3b1569bc41a595ea121161e12133ba707651d163c05a240';
@@ -15,6 +16,7 @@ router.get('/', async (ctx, next) => {
   ctx.body = json;
  });
 });
+app.use(cors({origin: '*'}));
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(PORT);
