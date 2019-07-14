11f72dd5848e4746d2fbdd4351b024f479b8bd0c
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:43:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000
 const apiKey = 'live_c4cf0b8b2a095663e3b1569bc41a595ea121161e12133ba707651d163c05a240';
 
-router.get('/:email', (ctx, next) => {
- const email = ctx.request.email;
+router.get('/', (ctx, next) => {
+ const email = ctx.request.query.email;
  console.log(email);
  const url = `https://api.kickbox.com/v2/verify?email=${email}&apikey=${apiKey}`;
  return fetch(url).then(res => res.json()).then(json => {
