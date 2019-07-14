c4a2edbad2054fc13c5d6a2d96bdb089efcd6441
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:27:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000
-const kickbox = require('kickbox').client('test_268aa7aa584bbc7191a5bbd10a8d8fb9bea6ac5592f1e9456692eda73f082ccf').kickbox();
+const apiKey = 'test_268aa7aa584bbc7191a5bbd10a8d8fb9bea6ac5592f1e9456692eda73f082ccf';
 
 router.get('/', async (ctx, next) => {
  const email = ctx.request.email;
- return kickbox.verify(email, (err, res) => {
+ const url = `https://api.kickbox.com/v2/verify?email=${email}&apikey=${apiKey}`;
+ return fetch(url).then(res => {
   console.log(res.body);
   ctx.body = res.body;
  });
