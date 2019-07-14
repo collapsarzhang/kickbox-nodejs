0e59bc207609e3ac2fc61b44226d8e5fe6efc373
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 22:57:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Koa = require('koa'); 
 const app = new Koa(); 
 app.use(async ctx => { ctx.body = 'Hello World'; });
-app.listen(80);
+app.listen(80, () => {
+  console.log('app started');
+});
