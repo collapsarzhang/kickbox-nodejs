21694f75c0f07f256d26be2d37c131e8d9d4eb9e
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:09:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 const PORT = process.env.PORT || 5000
+const kickbox = require('kickbox').client('test_268aa7aa584bbc7191a5bbd10a8d8fb9bea6ac5592f1e9456692eda73f082ccf').kickbox();
+
+kickbox.verify("test@example.com", function (err, response) {
+  // Let's see some results
+  console.log(response.body);
+});
 
 router.get('/', (ctx, next) => {
- ctx.body = 'Hello World!';
+ const email = ctx.request.email;
+ kickbox.verify(email, (err, res) => {
+  console.log(res.body);
+  ctx.body = res.body;
+ });
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
