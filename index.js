f312ecb72190ffa44213b54302f7c6bbce01ccd4
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:11:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const router = new Router();
 const PORT = process.env.PORT || 5000
 const kickbox = require('kickbox').client('test_268aa7aa584bbc7191a5bbd10a8d8fb9bea6ac5592f1e9456692eda73f082ccf').kickbox();
 
-kickbox.verify("test@example.com", function (err, response) {
-  // Let's see some results
-  console.log(response.body);
-});
-
 router.get('/', (ctx, next) => {
  const email = ctx.request.email;
  kickbox.verify(email, (err, res) => {
