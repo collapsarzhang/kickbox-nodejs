a0451efc4f886126036cacc8bcb6dd85a58f6e13
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:34:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const apiKey = 'live_c4cf0b8b2a095663e3b1569bc41a595ea121161e12133ba707651d163c0
 
 router.get('/', async (ctx, next) => {
  const email = ctx.request.email;
+ console.log(email);
  const url = `https://api.kickbox.com/v2/verify?email=${email}&apikey=${apiKey}`;
  return fetch(url).then(res => res.json()).then(json => {
   console.log(json);
