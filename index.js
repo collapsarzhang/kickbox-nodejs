85f5d9b31ee08c067bc940fc64cc03c82f5202a9
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2019-07-13 23:04:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const PORT = process.env.PORT || 5000
+
 router.get('/', (ctx, next) => {
  ctx.body = 'Hello World!';
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(8080);
+app.listen(PORT);
